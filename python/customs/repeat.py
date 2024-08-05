f3ff3e4614ac4fe3ac7ac401602a96835815e3ff
Jooeon Kim <joo2theeon@gmail.com>
2024-08-05 10:26:03 +0900

    [Customs/repeat] fix repeat_every & add arepeat

--- python/customs/repeat.py
+++ python/customs/repeat.py
@@ -2,10 +2,10 @@ import asyncio
 import logging
 
 from asyncio import ensure_future
-from typing import Callable
+from typing import Callable, Container
 
 
-def repeat(
+def repeat_every(
     *,
     seconds: float = 1,
     wait_first: bool = False,
@@ -47,7 +47,7 @@ def repeat(
                         await func(*args, **kwargs)
                     else:
                         func(*args, **kwargs)
-
+                    
                 except Exception as e:
                     if logger is not None:
                         logger.exception(e)
@@ -57,10 +57,63 @@ def repeat(
                 repetitions += 1
 
                 await asyncio.sleep(seconds)
-            
-            ensure_future(repeat(*args, **kwargs))
         
         return repeat
 
     return decorator
+
+
+async def arepeat(
+    func: Callable,
+    *,
+    seconds: float,
+    wait_first: bool = False,
+    logger: logging.Logger = None,
+    raise_exceptions: bool = False,
+    max_repetitions: int = None,
+    args: Container = (),
+    kwargs: dict = {},
+):
+    """
+    This function returns a decorator that schedules a function to execute periodically 
+    after every `seconds` seconds.
+
+    Parameters
+    ----------
+    seconds : float
+        The number of seconds to wait before executing the function again.
+    wait_first : bool, optional
+        Whether to wait `seconds` seconds before executing the function for the first 
+        time, by default False.
+    logger : logging.Logger, optional
+        The logger to use for logging exceptions, by default None.
+    raise_exceptions : bool, optional
+        Whether to raise exceptions instead of logging them, by default False.
+    max_repetitions : int, optional
+        The maximum number of times to repeat the function. If None, the function will 
+        repeat indefinitely, by default None.
+    """
+    is_coroutine = asyncio.iscoroutinefunction(func)
+
+    repetitions = 0
+
+    if wait_first:
+        await asyncio.sleep(seconds)
+    
+    while max_repetitions is None or repetitions < max_repetitions:
+        try:
+            if is_coroutine:
+                await func(*args, **kwargs)
+            else:
+                await asyncio.to_thread(func, *args, **kwargs)
+
+        except Exception as e:
+            if logger is not None:
+                logger.exception(e)
+            if raise_exceptions:
+                raise e
+        
+        repetitions += 1
+        
+        await asyncio.sleep(seconds)
     
--- python/customs/test_repeat.py
+++ python/customs/test_repeat.py
@@ -1,12 +1,25 @@
 import asyncio
 
-from repeat import repeat
+from repeat import repeat_every, arepeat
 
 
-@repeat(seconds=1)
-async def aprint(a):
+@repeat_every(seconds=1, max_repetitions=3)
+async def aprint_decorated(a):
     print(a)
 
 
+async def aprint(a, b=None):
+    print(a, b)
+
+
 if __name__ == "__main__":
-    asyncio.run(aprint("Hello, World!"))
+    asyncio.run(aprint_decorated("Hello, World!"))
+    asyncio.run(
+        arepeat(
+            aprint,
+            seconds=1,
+            max_repetitions=3,
+            args=["This is a test for arepeat!"],
+            kwargs={"b": "This is a test for arepeat!"},
+        )
+    )
