e9d7a89bed53127fafd853cb7cf5963fc6821222
Jooeon Kim <joo2theeon@gmail.com>
2024-08-28 14:49:22 +0900

    [Customs/repeat] enhance test

--- python/customs/test_repeat.py
+++ python/customs/test_repeat.py
@@ -13,7 +13,9 @@ async def aprint(a, b=None):
 
 
 if __name__ == "__main__":
+    # test repeat_every
     asyncio.run(aprint_decorated("Hello, World!"))
+    # test arepeat for a async function
     asyncio.run(
         arepeat(
             aprint,
@@ -21,5 +23,36 @@ if __name__ == "__main__":
             max_repetitions=3,
             args=["This is a test for arepeat!"],
             kwargs={"b": "This is a test for arepeat!"},
+        )
+    )
+    # test arepeat for a sync function
+    asyncio.run(
+        arepeat(
+            print,
+            seconds=1,
+            max_repetitions=3,
+            args=["This is a test for arepeat with a sync function!"],
+        )
+    )
+    # run in async gather
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        asyncio.gather(
+            aprint_decorated("Hello, World!"),
+            arepeat(
+                aprint,
+                seconds=1,
+                max_repetitions=3,
+                args=["This is a test for arepeat!"],
+                kwargs={"b": "This is a test for arepeat!"},
+            ),
+            arepeat(  # this works as async even though it is a sync function
+                print,
+                seconds=1,
+                max_repetitions=3,
+                args=["This is a test for arepeat with a sync function!"],
+            ),
         )
     )
